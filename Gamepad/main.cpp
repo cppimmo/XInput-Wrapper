d7de154b348d6f0156c49530b837e6a0c71d9f79
ImmortalityBH <ImmortalityBH@gmail.com>
2020-07-15 00:46:16 -0500

    fixed printing

--- Gamepad/main.cpp
+++ Gamepad/main.cpp
@@ -1,10 +1,8 @@
-
 #include <iostream>
 #include <chrono>
 #include <thread>
 #include <iomanip>
 #include "Gamepad.h"
-
 
 int main()
 {
@@ -21,14 +19,15 @@ int main()
         
         if (gamepad.Update())
         {
-            std::cout << std::fixed << std::setprecision(2) << "Left Trigger: " << gamepad.leftTrigger
-                << "\r\n" << std::flush;
-            
-            std::cout << "Left Stick " << "X: " << gamepad.leftStickX
-                << ", Y: " << gamepad.leftStickY << "\r\n" << std::flush;
-            
-            std::cout << "Right Stick " << "X: " << gamepad.rightStickX
-                << ", Y: " << gamepad.rightStickY << "\r\n" << std::flush;
+            system("cls");
+            std::cout << std::fixed << std::setprecision(2) << "Left Trigger: " << gamepad.leftTrigger <<
+                ", Right Trigger: " << gamepad.rightTrigger << "\n" <<
+
+            "Left Stick " << "X: " << gamepad.leftStickX
+            << ", Y: " << gamepad.leftStickY << "\n" <<
+
+            "Right Stick " << "X: " << gamepad.rightStickX
+            << ", Y: " << gamepad.rightStickY << std::endl;
 
             if (gamepad.isButtonPressed(XINPUT_GAMEPAD_A))
             {
@@ -47,6 +46,9 @@ int main()
             {
                 gamepad.resetVibration();
             }
+
+            if (gamepad.isButtonPressed(XINPUT_GAMEPAD_BACK))
+                break;
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
